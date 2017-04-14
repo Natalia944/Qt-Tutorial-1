d64f144e156b8f1ed3eb9f89f75ac70f46d22593
Natalia <Natalia944@onet.eu>
2017-04-14 16:18:24 +0200

    Notatnik

--- T1/mainwindow.h
+++ T1/mainwindow.h
@@ -15,8 +15,20 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void otworz_plik();  // otwieranie pliku
+    void zapisz_plik(QString = ""); // zapisywanie pliku
+    void zamien();
+
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void on_przycisk_clicked();
+    void on_actionOtw_rz_triggered();
+
+    void on_actionZapisz_triggered();
+    void on_actionZapisz_jako_triggered();
+    void on_actionZamknij_triggered();
 };
 
 #endif // MAINWINDOW_H
